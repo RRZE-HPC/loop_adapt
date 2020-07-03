fb4a80b05d3d342567da5f2ef32130bdff282542
Thomas Roehl <thomas.roehl@fau.de>
2020-07-03 11:24:58 +0200

    Export tree deletion function

--- include/loop_adapt_hwloc_tree.h
+++ include/loop_adapt_hwloc_tree.h
@@ -4,6 +4,7 @@
 #include <hwloc.h>
 
 int loop_adapt_copy_hwloc_tree(hwloc_topology_t* copy);
+void loop_adapt_copydestroy_hwloc_tree(hwloc_topology_t tree);
 void loop_adapt_hwloc_tree_finalize();
 
 #endif /* LOOP_ADAPT_HWLOC_TREE_H */
